1355d99b436459e36548bf04d9eab08b02db4b57
morningstar <75133174+moonstar-star@users.noreply.github.com>
2021-01-20 21:23:13 +0530

    Add files via upload

---
+++ CarRacingStage1.5/sketch.js
@@ -0,0 +1,41 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+var car1Img,car2Img,car3Img,car4Img,track 
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+function preload(){
+car1Img = loadImage("images/car1.png")
+car2Img = loadImage("images/car2.png")
+car3Img = loadImage("images/car3.png")
+car4Img = loadImage("images/car4.png")
+track = loadImage("images/track.jpg")
+
+}
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+if(gameState === 2){
+  game.end()
+}
+}
